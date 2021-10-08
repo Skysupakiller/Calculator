8cfb09c59eb1d3db57bcd2b92baed914db740b52
chris <chris.turp1@gmail.com>
2021-10-08 22:14:55 +0200

    bmi calculator added

--- calculator.js
+++ calculator.js
@@ -12,6 +12,21 @@ app.listen(3000,function(){
 app.get("/",function(req,res){
   res.sendFile(__dirname + "/index.html");
 })
+
 app.post("/",function(req,res){
-  res.send("Thank you for posting this");
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+  res.send("the result is " + result);
+})
+
+app.post("/bmicalculator",function(req,res){
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var yourBMI = weight / Math.pow(height,2);
+  res.send("Your BMI is " + yourBMI);
+})
+
+app.get("/bmicalculator", function(req,res){
+  res.sendFile(__dirname + "/bmicalculator.html");
 })
