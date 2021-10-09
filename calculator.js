9f16d72993adeedd98948212c8a83a5021a0f201
chris <chris.turp1@gmail.com>
2021-10-09 22:30:40 +0200

    Adding calculation

--- calculator.js
+++ calculator.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app=express();
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("I am listening on 3000");
 });
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
-app.post("/",function(req,res){
+app.post("/", function(req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
   res.send("the result is " + result);
-})
+});
 
-app.post("/bmicalculator",function(req,res){
+app.post("/bmicalculator", function(req, res) {
   var weight = Number(req.body.weight);
   var height = Number(req.body.height);
-  var yourBMI = weight / Math.pow(height,2);
+  var yourBMI = weight / Math.pow(height, 2);
   res.send("Your BMI is " + yourBMI);
-})
+});
 
-app.get("/bmicalculator", function(req,res){
+app.get("/bmicalculator", function(req, res) {
   res.sendFile(__dirname + "/bmicalculator.html");
-})
+});
